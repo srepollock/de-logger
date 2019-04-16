95b1d19e067ef2db06853c9d22a9e7d2e1aeffc6
Spencer Pollock <srepollock@gmail.com>
2019-04-15 23:35:06 -0700

    feat(errorcode.ts): Added Divine Engine error codes

--- src/errorcode.ts
+++ src/errorcode.ts
@@ -3,6 +3,61 @@
  */
 export enum ErrorCode {
     OK = 0,
-    Error = 1, // General Errors
-    // Extend your error codes here
+    EngineFailed = 1,
+    Error, // General Errors
+    DocumentUndefined,
+    EngineInitialization = 100, // Engine Errors
+    EngineInstanceUndefined,
+    EngineInstanceNotUndefined,
+    EngineClientNotSet,
+    EngineWindowUndefined,
+    EngineStartedEarly,
+    EngineRunning,
+    EngineNotRunning,
+    EngineCleanupFailed,
+    MessageSystemInitialization = 200, // MessageSystem Errors
+    MessageSystemUndefined,
+    DuplicateListener,
+    UnsubscribeFailed,
+    MessageRecieverNotFound,
+    FailedAddingListener,
+    ListenerUndefined,
+    BrowserWindowUndefined = 300, // BrowserWindow Errors
+    BrowserWindowDidNotClose,
+    ContainerUndefined,
+    SceneUndefined = 400, // Scene Errors
+    SceneNameUndefined,
+    SceneManagerUndefined,
+    SceneManagerCleanupFailed,
+    SceneNotLoaded,
+    EntityInitialization = 500, // Entity Errors
+    EntityParentUndefined,
+    EntityAlreadyHasChild,
+    EntityAlreadyHasComponent,
+    EntityChildNotFound,
+    EntityComponentNotFound,
+    RenderSystemUndefined = 600, // RenderSystem Errors
+    RenderSystemInitializationFailed,
+    CanvasNotFound,
+    RenderSystemCleanupFailed,
+    PhysicsSystemUndefined = 700, // PhysicsSystem Error
+    WindowUndefined, // Window Errors
+    GameWindowUndefined,
+    AssetLoaderUninitialized, // Asset Errors
+    AssetManagerUndefined,
+    NoFileExtension,
+    NoAssetName,
+    LoadAssetFailed,
+    JSONAssetNotLoaded,
+    JSONDataUndefined,
+    AssetManagerDidNotGetAsset,
+    JSONLoaderPathImport,
+    JSONLoaderFsImport,
+    JSONLoaderFsStat,
+    FileDoesNotExist,
+    FileDataUndefined,
+    ErrorLoadingFile = 800, // Helper Function Errors
+    ReadJSONFile,
+    WriteJSONFile,
+    FileContentsNotRead,
 }
