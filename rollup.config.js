fd49fda5ba746cf8c5aeb313307e4ae91e83c6e2
Spencer Pollock <srepollock@gmail.com>
2019-09-10 15:28:27 -0700

    feat: 🎸 Bundles everything into a single file for es or umd

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from "./package.json";
+
+let override = { compilerOptions: { declaration: false } };
+
+export default [
+    {
+        input: "./src/index.ts",
+        plugins: [
+            typescript({tsconfigOverride: override})
+        ],
+        output: [
+            {
+                file: pkg.module,
+                format: "es"
+            },
+            {
+                file: pkg.main,
+                name: "de-loggingsystem",
+                format: "umd"
+            }
+        ]
+    }
+]
